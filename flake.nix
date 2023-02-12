c7bd6e98600b7b325da9e51b5ab1e84c9a279ab8
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-02-12 18:43:20 +0000

    flake.nix: support different systems; simplify
    mach-nix doesn't support multiple systems, and we don't need it anyway.

--- flake.nix
+++ flake.nix
@@ -5,36 +5,36 @@
     plus utilities
     '';
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-    flake-utils.url = github:numtide/flake-utils;
-    mach-nix.url = "mach-nix/3.4.0";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, mach-nix }:
-    let
-      system = "x86_64-linux";
-      machNix = mach-nix.lib."${system}";
-      tuzue = machNix.buildPythonApplication {
-        src = self;
-      };
-    in {
-      packages.${system} = {
-        inherit tuzue;
-        default = tuzue;
-      };
 
-      apps.${system} = {
-        tuzue-chdir = {
-          type = "app";
-          program = "${tuzue}/bin/tuzue-chdir";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        tuzue = pkgs.python3Packages.buildPythonApplication {
+          pname = "tuzue";
+          version = "0.1.3";
+          src = self;
         };
-        tuzue-json = {
-          type = "app";
-          program = "${tuzue}/bin/tuzue-json";
+      in
+      rec {
+        packages.default = tuzue;
+        apps.default = {
+          tuzue-chdir = {
+            type = "app";
+            program = "${tuzue}/bin/tuzue-chdir";
+          };
+          tuzue-json = {
+            type = "app";
+            program = "${tuzue}/bin/tuzue-json";
+          };
+          tuzue-manmenu = {
+            type = "app";
+            program = "${tuzue}/bin/tuzue-manmenu";
+          };
         };
-        tuzue-manmenu = {
-          type = "app";
-          program = "${tuzue}/bin/tuzue-manmenu";
-        };
-      };
-    };
+      }
+    );
 }
